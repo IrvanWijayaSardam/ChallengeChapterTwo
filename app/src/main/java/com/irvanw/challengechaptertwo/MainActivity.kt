a66f88d0871c4015c463996c299e33c44a62cf0d
MSI\Irvan Wijaya <irfanwijayasardam@gmail.com>
2022-08-27 12:20:58 +0700

    Getting value from edt cost of service before get changed , and creating dialogue for thanks after submiting tip

--- app/src/main/java/com/irvanw/challengechaptertwo/MainActivity.kt
+++ app/src/main/java/com/irvanw/challengechaptertwo/MainActivity.kt
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 import androidx.cardview.widget.CardView
 import androidx.core.view.isVisible
+import androidx.fragment.app.DialogFragment
 import java.lang.ref.WeakReference
 import java.text.DecimalFormat
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSubmit: Button = findViewById(R.id.btnSubmit)
 
         val cvBtnSatu = findViewById<CardView>(R.id.rbSatu)
         val cvBtnDua = findViewById<CardView>(R.id.rbDua)
@@ -25,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         val radioGrp = findViewById<RadioGroup>(R.id.rGroup)
 
+        var moneyReal : String
 
         val selectedAmazing : ImageView = findViewById(R.id.ivAmazing)
         val selectedGood : ImageView = findViewById(R.id.ivGood)
@@ -33,6 +41,27 @@ class MainActivity : AppCompatActivity() {
         val edtMoney : EditText = findViewById(R.id.edtCOS)
 
         edtMoney.setMaskingMoney("Rp. ")
+
+
+        val strEdtMoney = edtMoney.setMaskingMoney("Rp. ")
+
+
+        btnSubmit.setOnClickListener(){
+            if (edtMoney.length() == 0) {
+                Toast.makeText(this, "0 Money", Toast.LENGTH_SHORT).show()
+            } else {
+                if(selectedAmazing.isVisible){
+                    removeKoma(edtMoney.text.toString())
+                    MyCustomDialog().show(supportFragmentManager, "MyCustomFragment")
+                } else if(selectedGood.isVisible){
+                    Toast.makeText(this, "Selected good", Toast.LENGTH_SHORT).show()
+                } else if(selectedOk.isVisible){
+                    Toast.makeText(this, "Selected Ok", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Kamu tidak akan memberikan tips ? ", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
         cvBtnSatu.setOnClickListener{
             selectedGood.isVisible = false
@@ -63,6 +92,7 @@ class MainActivity : AppCompatActivity() {
                 selectedOk.isVisible = true
             }
         }
+
     }
     interface MyTextWatcher: TextWatcher {
         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
@@ -72,7 +102,7 @@ class MainActivity : AppCompatActivity() {
     fun String.monetize(): String = if (this.isEmpty()) "0"
     else DecimalFormat("#,###").format(this.replace("[^\\d]".toRegex(),"").toLong())
 
-    fun EditText.setMaskingMoney(currencyText: String) {
+    fun EditText.setMaskingMoney(currencyText: String){
         this.addTextChangedListener(object: MyTextWatcher{
             val editTextWeakReference: WeakReference<EditText> = WeakReference<EditText>(this@setMaskingMoney)
             override fun afterTextChanged(editable: Editable?) {
@@ -85,9 +115,42 @@ class MainActivity : AppCompatActivity() {
                 editText.setText(newval)
                 editText.setSelection(newval.length)
                 editText.addTextChangedListener(this)
+                
             }
         })
     }
 
+    fun hitungTIp(cos : Int,presentase:Double):Int{
+        var hasilHitung : Int
+
+        hasilHitung = (cos / presentase).roundToInt()
+
+        return hasilHitung
+    }
+
+    fun removeKoma(oldString : String): String{
+        var rp : String = oldString
+        var lenghthRp = rp.length
+        var withoutRp = rp.substring(4,lenghthRp)
+        var withtoutComa:String = withoutRp.replace(",","")
+        Toast.makeText(this, "${withtoutComa}", Toast.LENGTH_SHORT).show()
+        return  withtoutComa
+    }
+
+}
+
+class MyCustomDialog: DialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.round_corner);
+        return inflater.inflate(R.layout.dialogue_thanks, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
+        val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
+        dialog!!.window?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
 
 }
